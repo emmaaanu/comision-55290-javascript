57eaf0b9324723414bad164648aa0074f6f06725
emmanuel roman <emmaaanu9@gmail.com>
2023-08-15 11:46:37 -0300

    tercera-pre-entrega

--- js/script.js
+++ js/script.js
@@ -52,53 +52,13 @@ const nombresGatos = {
     "Lia",
   ],
 };
-// Función para obtener una entrada del usuario y validarla
-function obtenerEntradaUsuario(mensaje, opcionesValidas) {
-  let entrada = "";
 
-  while (!opcionesValidas.includes(entrada)) {
-    entrada = prompt(mensaje);
-
-    if (entrada === null) {
-      console.log("No se seleccionó ninguna opción.");
-      return null;
-    } else {
-      entrada = entrada.toUpperCase();
-    }
-
-    if (!opcionesValidas.includes(entrada)) {
-      console.log("Opción no válida.");
-    }
-  }
-
-  return entrada;
-}
-// Función para obtener el tipo de mascota (perro o gato)
-function obtenerTipoMascota() {
-  const tipoMascota = obtenerEntradaUsuario(
-    "¿Qué tipo de mascota deseas generar un nombre? (P) Perro | (G) Gato",
-    ["P", "G"]
-  );
-
-  return tipoMascota === "P" ? "perro" : "gato";
-}
-// Función para obtener el género de la mascota (macho o hembra)
-function obtenerGeneroMascota() {
-  const generoMascota = obtenerEntradaUsuario(
-    "¿Cuál es el género de la mascota? (M) Macho | (H) Hembra",
-    ["M", "H"]
-  );
-
-  return generoMascota === "M" ? "macho" : "hembra";
-}
 // Función para generar nombres únicos aleatorios
-function generarNombresUnicos(nombres, cantidad) {
+function generarNombresUnicos(nombres, genero, cantidad) {
   const nombresGenerados = new Set();
-  const generos = Object.keys(nombres);
+  const nombresPorGenero = nombres[genero];
 
   while (nombresGenerados.size < cantidad) {
-    const generoAleatorio = generos[Math.floor(Math.random() * generos.length)];
-    const nombresPorGenero = nombres[generoAleatorio];
     const nombreAleatorio =
       nombresPorGenero[Math.floor(Math.random() * nombresPorGenero.length)];
 
@@ -107,40 +67,49 @@ function generarNombresUnicos(nombres, cantidad) {
 
   return [...nombresGenerados];
 }
-// Función para generar nombres de mascotas
+
 function generarNombresMascotas(tipoMascota, generoMascota, cantidadNombres) {
   const nombres = tipoMascota === "perro" ? nombresPerros : nombresGatos;
-  const nombresGenerados = generarNombresUnicos(nombres, cantidadNombres);
+  const nombresGenerados = generarNombresUnicos(
+    nombres,
+    generoMascota,
+    cantidadNombres
+  );
 
   return nombresGenerados;
 }
-// Función para manejar el simulador interactivo
-function simuladorNombresMascotas() {
-  const tipoMascota = obtenerTipoMascota();
 
-  if (tipoMascota) {
-    const generoMascota = obtenerGeneroMascota();
+function simuladorNombresMascotas(cardElement) {
+  const tipoMascota = cardElement.classList.contains("perro")
+    ? "perro"
+    : "gato";
+  const generoMascota = cardElement.querySelector(".genero-mascota").value;
+  const cantidadNombresNum = parseInt(
+    cardElement.querySelector(".cantidad-nombres").value
+  );
 
-    if (generoMascota) {
-      let cantidadNombresNum = NaN;
-
-      while (isNaN(cantidadNombresNum) || cantidadNombresNum <= 0) {
-        const cantidadNombres = prompt("¿Cuántos nombres deseas generar?");
-        cantidadNombresNum = parseInt(cantidadNombres);
-
-        if (isNaN(cantidadNombresNum) || cantidadNombresNum <= 0) {
-          console.log("Ingresa una cantidad válida.");
-        }
-      }
-
-      const nombresGenerados = generarNombresMascotas(
-        tipoMascota,
-        generoMascota,
-        cantidadNombresNum
-      );
-      console.log(`Nombres generados: ${nombresGenerados.join(", ")}`);
-    }
+  if (!isNaN(cantidadNombresNum) && cantidadNombresNum > 0) {
+    const nombresGenerados = generarNombresMascotas(
+      tipoMascota,
+      generoMascota,
+      cantidadNombresNum
+    );
+    const resultadosElement = cardElement.querySelector(".resultados");
+    resultadosElement.innerHTML = `Nombres generados: ${nombresGenerados.join(
+      ", "
+    )}`;
+  } else {
+    console.log("Ingresa una cantidad válida.");
   }
 }
 
-simuladorNombresMascotas();
+const perroCard = document.querySelector(".card.perro");
+const gatoCard = document.querySelector(".card.gato");
+
+perroCard
+  .querySelector(".generar-nombres")
+  .addEventListener("click", () => simuladorNombresMascotas(perroCard));
+
+gatoCard
+  .querySelector(".generar-nombres")
+  .addEventListener("click", () => simuladorNombresMascotas(gatoCard));
